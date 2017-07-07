d1ec08ec01b203ca263bb4a48bc66888065dfa30
Антон Степаненков <Lenseg1@gmail.com>
2017-07-07 23:13:37 +0300

    finita

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,6 +14,8 @@ export class HomePage {
   eventSource:any={};
   constructor(public navCtrl: NavController) {
     this.calendar.currentDate = new Date();
+    let fewDaysBefore = new Date();;
+    fewDaysBefore.setDate(fewDaysBefore.getDate() - 3);
     this.calendar.showEventDetail = false;
     this.eventSource=[
       {
@@ -23,15 +25,27 @@ export class HomePage {
         allDay:true,
         url:'/kek',
         location:'Комсомольская 27',
+        liked:true,
         description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         pinColor:'#9c28af'
       },{
         title:'Медитация на банан',
-        startTime:this.calendar.currentDate,
-        endTime:this.calendar.currentDate,
+        startTime:fewDaysBefore,
+        endTime:fewDaysBefore,
         allDay:true,
         url:'/kek',
         location:'Комсомольская 27',
+        liked:false,
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        pinColor:'#ff9600'
+      },{
+        title:'Охота на тигров',
+        startTime:fewDaysBefore,
+        endTime:fewDaysBefore,
+        allDay:true,
+        url:'/kek',
+        location:'Комсомольская 27',
+        liked:false,
         description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         pinColor:'#ff9600'
       },{
@@ -41,6 +55,7 @@ export class HomePage {
         allDay:true,
         url:'/kek',
         location:'Комсомольская 27',
+        liked:true,
         description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         pinColor:'#5e35af'
       },{
@@ -50,6 +65,7 @@ export class HomePage {
         allDay:true,
         url:'/kek',
         location:'Комсомольская 27',
+        liked:true,
         description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         pinColor:'#7db343'
       }
